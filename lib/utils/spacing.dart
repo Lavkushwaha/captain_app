92df7e1b7e83edb2a0df3412c0129ad85b9562e9
Lavkant Kachhwaha <kush373@gmail.com>
2022-02-23 22:01:15 +0530

    spacing added for minimal changes in the app

---
+++ lib/utils/spacing.dart
@@ -0,0 +1,12 @@
+class Spacing {
+  Spacing._();
+
+  // static const double xxs = 5.0;
+  static const double xxs = 2.0;
+  static const double xs = 4.0;
+  static const double s = 8.0;
+  static const double m = 14.0;
+  static const double l = 16.0;
+  static const double xl = 18.0;
+  static const double xxl = 22.0;
+}
