c9c43a0e5e9471a9d2f1cc0fc65de70ebc34cc08
Lavkant Kachhwaha <kush373@gmail.com>
2022-02-23 21:57:51 +0530

    demo data added for rendering lists

---
+++ lib/shared/data/list_data.dart
@@ -0,0 +1,44 @@
+final list_data = [
+  {
+    "id": 1,
+    "img_url": "https://pngimg.com/uploads/samoyed_dog/samoyed_dog_PNG32.png",
+    "name": "Dog",
+    "net_wt": "80.2",
+    "gross_wt": "8.5"
+  },
+  {
+    "id": 2,
+    "img_url": "https://pngimg.com/uploads/samoyed_dog/samoyed_dog_PNG32.png",
+    "name": "Dog 2",
+    "net_wt": "50.0",
+    "gross_wt": "52.0"
+  },
+  {
+    "id": 3,
+    "img_url": "https://pngimg.com/uploads/samoyed_dog/samoyed_dog_PNG3.png",
+    "name": "Dog 3",
+    "net_wt": "500.0",
+    "gross_wt": "500.8"
+  },
+  {
+    "id": 4,
+    "img_url": "https://pngimg.com/uploads/panda/panda_PNG13.png",
+    "name": "Panda",
+    "net_wt": "100.0",
+    "gross_wt": "100.0"
+  },
+  {
+    "id": 5,
+    "img_url": "https://pngimg.com/uploads/lobster/lobster_PNG14255.png",
+    "name": "Lobster",
+    "net_wt": "498.0",
+    "gross_wt": "500.0"
+  },
+  {
+    "id": 6,
+    "img_url": "https://pngimg.com/uploads/fish/fish_PNG25153.png",
+    "name": "Gold fish",
+    "net_wt": "0.5",
+    "gross_wt": "0.5"
+  }
+];
