3e103114445d5447c0e96c67c78bab6fa493a21b
Lavkant Kachhwaha <kush373@gmail.com>
2022-02-23 22:39:25 +0530

    move to home function added

--- lib/blocs/splash_bloc/splash_bloc.dart
+++ lib/blocs/splash_bloc/splash_bloc.dart
@@ -1,6 +1,17 @@
 import 'package:captain_app/blocs/base_bloc.dart';
+import 'package:captain_app/routes.dart';
+import 'package:captain_app/shared/services/navigation_service.dart';
 
 class SplashBloc extends Bloc {
+  _moveToHome() {
+    Future.delayed(const Duration(seconds: 3), () {
+      appNavigationService.pushReplacementNamed(Routes.home);
+    });
+  }
+
+  Function get moveToHome => _moveToHome;
   @override
   void dispose() {}
 }
+
+final splashBloc = SplashBloc();
