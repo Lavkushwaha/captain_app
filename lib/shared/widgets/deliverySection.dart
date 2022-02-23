e24b016beda5d1aff1c82b4528b57f54d6250168
Lavkant Kachhwaha <kush373@gmail.com>
2022-02-23 21:59:07 +0530

    delivery section added in shared

---
+++ lib/shared/widgets/deliverySection.dart
@@ -0,0 +1,76 @@
+import 'package:captain_app/blocs/address_bloc/address_bloc.dart';
+import 'package:captain_app/shared/widgets/showBottomSheet.dart';
+import 'package:captain_app/utils/colors.dart';
+import 'package:captain_app/utils/helper_functions.dart';
+import 'package:captain_app/utils/spacing.dart';
+import 'package:flutter/material.dart';
+
+class DeliverySection extends StatelessWidget {
+  const DeliverySection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        showModalBottomSheet<void>(
+          context: context,
+          builder: (BuildContext context) {
+            return const BottomSheetContainer();
+          },
+        );
+      },
+      child: StreamBuilder<bool>(
+          stream: addressBloc.loadingStream,
+          builder: (context, snapshot) {
+            if (snapshot.hasData && snapshot.data == true) {
+              return const SizedBox(
+                height: 50,
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            }
+            return Container(
+              height: 50,
+              width: double.infinity,
+              color: AppColors.greyColor,
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Delivey Date : ",
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.normal,
+                          // color: AppColors.whiteColor
+                        )),
+                    Text(
+                        " ${HelperFunctions.getDateTime(addressBloc.deliveryDate)}",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          // color: AppColors.whiteColor
+                        )),
+                    const SizedBox(
+                      width: Spacing.m,
+                    ),
+                    Text("Pin Code :",
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.normal,
+                          // color: AppColors.whiteColor
+                        )),
+                    Text(" ${addressBloc.pincodeController.text}",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          // color: AppColors.whiteColor
+                        )),
+                  ],
+                ),
+              ),
+            );
+          }),
+    );
+  }
+}
