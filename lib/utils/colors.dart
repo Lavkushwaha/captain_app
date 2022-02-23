73b37a5adae783e3ed3cd0ee5d756b826a5b7c0b
Lavkant Kachhwaha <kush373@gmail.com>
2022-02-23 22:00:25 +0530

    colors added

---
+++ lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static Color greyColor = const Color(0xFFDDDDDD).withOpacity(0.8);
+  static const Color orangeColor = Color(0xFFF26B26);
+  static const Color lightOrangeColor = Color(0xFFFAEAE4);
+}
