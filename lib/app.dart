be449da7af30aa63e637b25736e25d79d8de4ba0
Lavkant Kachhwaha <kush373@gmail.com>
2022-02-24 14:12:51 +0530

    dispose function added for bloc in app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,34 @@
+import 'package:captain_app/blocs/address_bloc/address_bloc.dart';
+import 'package:captain_app/blocs/cart_bloc/cart_bloc.dart';
+import 'package:captain_app/blocs/home_bloc/home_bloc.dart';
+import 'package:captain_app/blocs/splash_bloc/splash_bloc.dart';
 import 'package:captain_app/config/app_config.dart';
 import 'package:captain_app/routes.dart';
 import 'package:captain_app/shared/services/navigation_service.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+
+//DISPOSE FUNCTION ADDED FOR BLOCS
+  @override
+  void dispose() {
+    homeBloc.dispose();
+    cartBloc.dispose();
+    addressBloc.dispose();
+    splashBloc.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
